1e30911185f466fe5e1f30ff607489c807d84af0
Antoni Collado Folch <acfcoder@gmail.com>
2022-11-25 23:33:20 +0100

    Subida inicial

---
+++ src/assets/scripts/script.js
@@ -0,0 +1,17 @@
+//import jQuery
+
+import "./import-jquery.js" ;
+/*
+$(document).ready(function(){
+	$(".lines a").mouseover(function(){
+		$(li).css("background-color", "red");
+		}, function(){
+			$(this).append("hola");
+		});
+  $(".lines a").mouseout(function(){
+    $(this).css("background-color", "transparent");
+    }, function(){
+      $(this).css("background-color", "transparent");
+    });  
+});
+*/
